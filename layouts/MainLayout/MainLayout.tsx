db0954ab9ba089d00a99316b36457931029b33a7
Max Kalimullin <m.kalimullin@mediasoft.team>
2022-03-10 15:24:52 +0300

    create footer

--- layouts/MainLayout/MainLayout.tsx
+++ layouts/MainLayout/MainLayout.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { Container } from '@mui/material';
 
 import Header from 'components/base/header/Header';
+import { Footer } from 'components/main/Footer/Footer';
 
 const MainLayout: React.FC = ({ children }) => (
-  <Container
-    maxWidth={'lg'}
-    sx={{
-      minHeight: '100vh',
-    }}
-  >
-    <Header />
-    {children}
-  </Container>
+  <>
+    <Container
+      maxWidth={'lg'}
+      sx={{
+        minHeight: '100vh',
+      }}
+    >
+      <Header />
+      {children}
+    </Container>
+    <Footer />
+  </>
 );
 
 export { MainLayout };
