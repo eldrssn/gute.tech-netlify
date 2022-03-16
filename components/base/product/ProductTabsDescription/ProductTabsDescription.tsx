be81e7e0c439a42eabe367288c242849556749d1
Alexey <a.pruss@mediasoft.team>
2022-03-16 07:08:32 +0000

    Develop

--- components/base/product/ProductTabsDescription/ProductTabsDescription.tsx
+++ components/base/product/ProductTabsDescription/ProductTabsDescription.tsx
@@ -6,10 +6,11 @@ import { isMobileView } from 'utility/utils/isMobileView';
 import { TabsMobileView } from '../TabsMobileView';
 import { TabsDesktopView } from '../TabsDesktopView';
 
-import { TabsProps as Props } from 'types/productTypes';
+import { TabsProps } from 'types/productTypes';
 
-export const ProductTabsDescription: FC<Props> = ({ productInfo }) => {
+export const ProductTabsDescription: FC<TabsProps> = ({ productInfo }) => {
   const { windowWidth } = useWindowSize();
+
   const Component = isMobileView(windowWidth)
     ? TabsMobileView
     : TabsDesktopView;
--- components/main/header/HeaderNavMenu/types.ts
+++
@@ -1,7 +0,0 @@
-import { menuIcons } from './constants';
-
-export interface HeaderMenuProps {
-  isFullMenu?: boolean;
-}
-
-export type PagesType = keyof typeof menuIcons;
