4c5ff1e78d54375c73c79b94f72773b30df9fb99
Максим Калимуллин <m.kalimullin@mediasoft.team>
2022-05-26 09:25:39 +0000

    Fix gallery

--- components/base/product/ProductImageGallery/ProductImageGallery.tsx
+++ components/base/product/ProductImageGallery/ProductImageGallery.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useRef, useState } from 'react';
 
 import ImageGallery from 'react-image-gallery';
+import ReactImageGallery from 'react-image-gallery';
 import Box from '@mui/material/Box';
 
 import 'react-image-gallery/styles/css/image-gallery.css';
@@ -16,6 +17,21 @@ const ProductImageGallery: FC<ProductImageGalleryProps> = ({
   title,
 }) => {
   const [isFullscreen, setFullscreen] = useState<boolean>(false);
+  const [fullscreenIndex, setFullscreenIndex] = useState<number>(0);
+
+  const refImg = useRef<ReactImageGallery | null>(null);
+
+  const onSlide = (currentIndex: number) => {
+    setFullscreenIndex(currentIndex);
+  };
+
+  const slideToIndex = (index: number) => {
+    if (!refImg.current) {
+      return;
+    }
+
+    refImg.current.slideToIndex(index);
+  };
 
   if (!images) {
     return <Loader />;
@@ -23,26 +39,39 @@ const ProductImageGallery: FC<ProductImageGalleryProps> = ({
 
   const formattedItems = formatImages(images);
 
-  const toggleFullscreen = () => setFullscreen((isFullscreen) => !isFullscreen);
+  const toggleFullscreen = () => {
+    setFullscreen((isFullscreen) => !isFullscreen);
+  };
 
   return (
     <>
-      <Box sx={{ width: { xs: '100%', sm: '50%' } }}>
+      <Box
+        sx={{
+          width: { xs: '100%', sm: '50%' },
+          display: isFullscreen ? 'none' : 'block',
+        }}
+      >
         <ImageGallery
+          startIndex={fullscreenIndex}
+          ref={refImg}
           showPlayButton={false}
           items={formattedItems}
           onClick={toggleFullscreen}
           showNav={false}
           useBrowserFullscreen={false}
-          showFullscreenButton={false}
+          showFullscreenButton={isFullscreen}
+          onSlide={onSlide}
+          lazyLoad={true}
         />
       </Box>
 
       {isFullscreen && (
         <FullscreenGallery
           toggleFullscreen={toggleFullscreen}
+          fullscreenIndex={fullscreenIndex}
           images={images}
           title={title}
+          slideToIndex={slideToIndex}
         />
       )}
     </>
--- components/base/product/ProductImageGallery/types.ts
+++ components/base/product/ProductImageGallery/types.ts
@@ -7,6 +7,8 @@ type FullscreenGalleryProps = {
   images: string[];
   title: string;
   toggleFullscreen: () => void;
+  fullscreenIndex: number;
+  slideToIndex: (index: number) => void;
 };
 
 export type { ProductImageGalleryProps, FullscreenGalleryProps };
