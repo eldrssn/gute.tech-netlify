4c0d049fb32d146cc98f47722c48019b6bf0f655
Alexey Pruss <a.pruss@mediasoft.team>
2022-06-16 16:31:38 +0300

    add comments and build quick fix

--- api/utils.ts
+++ api/utils.ts
@@ -3,6 +3,9 @@ import axios, { AxiosRequestConfig } from 'axios';
 import { DEV_HOST } from 'constants/variables';
 
 const api = axios.create({
+  // FIXME: когда запустим stage, переезжаем на api-stage...
+  // саму строчку удалить и занести в .env.local локально
+  // NEXT_PUBLIC_API_URL=https://api-stage.gute.tech/api/v1
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://api-stage.gute.tech/api/v1',
   headers: {
     'content-type': 'application/json',
