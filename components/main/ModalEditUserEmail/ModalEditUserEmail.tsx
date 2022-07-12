2e5de6b6f950251935a7a341ed58494e2c2f2f01
Alexey Pruss <a.pruss@mediasoft.team>
2022-07-12 16:51:53 +0300

    fix

--- components/main/ModalEditUserEmail/ModalEditUserEmail.tsx
+++ components/main/ModalEditUserEmail/ModalEditUserEmail.tsx
@@ -48,7 +48,6 @@ const ModalEditUserEmail: React.FC<TOuterProps> = ({
     mode: 'onTouched',
     reValidateMode: 'onChange',
     criteriaMode: 'firstError',
-
     shouldFocusError: true,
   });
   const dispatch = useDispatch();
