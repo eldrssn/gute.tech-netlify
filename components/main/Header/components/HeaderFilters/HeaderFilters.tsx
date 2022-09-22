aac92bb68a42284fdd53c03d62e0312f0f872928
Максим Калимуллин <m.kalimullin@mediasoft.team>
2022-09-22 15:33:31 +0000

    Fixes

--- components/main/Header/components/HeaderFilters/HeaderFilters.tsx
+++ components/main/Header/components/HeaderFilters/HeaderFilters.tsx
@@ -4,17 +4,12 @@ import classnames from 'classnames/bind';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Divider from '@mui/material/Divider';
 import FormControl from '@mui/material/FormControl';
 
 import { useWindowSize } from 'hooks/useWindowSize';
 
 import { CatalogButton } from '../CatalogButton';
-import { HeaderLogo } from '../HeaderLogo';
-import { HeaderAsideNav } from '../HeaderAsideNav';
-
 import { HeaderContext } from '../HeaderContext';
-import { SearchField } from '../SearchField';
 import { HeaderFiltersForm } from '../HeaderFiltersForm';
 
 import { HeaderFiltersProps } from './types';
@@ -25,73 +20,48 @@ const cn = classnames.bind(styles);
 
 const HeaderFilters: FC<HeaderFiltersProps> = ({ closePopupMobile }) => {
   const { isMobile, isTablet } = useWindowSize();
-  const { isFullHeader, isFocusSearchField } = useContext(HeaderContext);
+  const { isFullHeader } = useContext(HeaderContext);
 
-  const isHiddenFilter = !isFullHeader && isFocusSearchField && !isTablet;
   const isFullDesktopHeader = isFullHeader && !isMobile;
   const isShortDesktopHeader = !isFullHeader && !isMobile;
 
   return (
-    <>
-      <Container className={styles.mainContainer}>
-        <Box
-          className={styles.itemsContainer}
-          sx={{
-            display: isFullHeader ? 'block' : 'flex',
-            flexWrap: { xs: 'wrap', lg: 'nowrap' },
-          }}
+    <Container
+      sx={{
+        order: { xs: 1, lg: 0 },
+        display: isFullHeader ? 'block' : 'flex',
+        flexWrap: { xs: 'wrap', lg: 'nowrap' },
+      }}
+      className={cn(styles.mainContainer, {
+        [styles.mainContainer_short]: isShortDesktopHeader,
+      })}
+    >
+      {isFullDesktopHeader && (
+        <Typography className={styles.filterText} component='p'>
+          Воспользуйтесь фильтром и сайт автоматически подберёт подходящие
+          детали для вашего транспорта
+        </Typography>
+      )}
+
+      <Box
+        className={styles.formAndCatalogContainer}
+        sx={{
+          order: { xs: 1, lg: 0 },
+          marginTop: { sm: '5px', lg: 0 },
+        }}
+      >
+        <FormControl
+          className={cn(styles.filterStepsForm, {
+            [styles.filterStepsForm_shortHeader]: !isFullHeader || isTablet,
+            [styles.filterStepsForm_mobileView]: isMobile,
+          })}
         >
-          {isFullDesktopHeader && (
-            <Typography className={styles.filterText} component='p'>
-              Воспользуйтесь фильтром и сайт автоматически подберёт подходящие
-              детали для вашего транспорта
-            </Typography>
-          )}
+          <HeaderFiltersForm closePopupMobile={closePopupMobile} />
+        </FormControl>
 
-          {!isHiddenFilter && (
-            <Box
-              className={styles.formAndCatalogContainer}
-              sx={{
-                order: { xs: 1, lg: 0 },
-                marginTop: { sm: '5px', lg: 0 },
-              }}
-            >
-              <FormControl
-                className={cn(styles.filterStepsForm, {
-                  [styles.filterStepsForm_shortHeader]:
-                    !isFullHeader || isTablet,
-                  [styles.filterStepsForm_mobileView]: isMobile,
-                })}
-              >
-                <HeaderFiltersForm closePopupMobile={closePopupMobile} />
-              </FormControl>
-
-              {isFullDesktopHeader && <CatalogButton />}
-            </Box>
-          )}
-
-          {isShortDesktopHeader && (
-            <>
-              <Box
-                sx={{
-                  order: -1,
-                  display: 'flex',
-                }}
-              >
-                {!isMobile && <HeaderLogo />}
-
-                <CatalogButton />
-              </Box>
-              <SearchField />
-            </>
-          )}
-
-          {isShortDesktopHeader && <HeaderAsideNav />}
-        </Box>
-      </Container>
-
-      {isShortDesktopHeader && <Divider />}
-    </>
+        {isFullDesktopHeader && <CatalogButton />}
+      </Box>
+    </Container>
   );
 };
 
