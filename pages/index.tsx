738b21ebfa2eb1e4ee64e40c0d0929743a321b3a
Максим Калимуллин <m.kalimullin@mediasoft.team>
2022-12-12 08:05:51 +0000

    Try fetch9

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,38 +8,7 @@ export const getServerSideProps = wrapper.getServerSideProps(
     const { req, res } = context;
     const isServer = req;
 
-    // context.res.
-    // res.writeHead();
-
-    const headers = [
-      {
-        key: 'Strict-Transport-Security',
-        value: 'max-age=63072000; includeSubDomains',
-      },
-      {
-        key: 'X-XSS-Protection',
-        value: '1; mode=block',
-      },
-      {
-        key: 'X-Content-Type-Options',
-        value: 'nosniff',
-      },
-      {
-        key: 'Permissions-Policy',
-        value: 'geolocation=()',
-      },
-      {
-        key: 'Referrer-Policy',
-        value: 'strict-origin-when-cross-origin',
-      },
-      {
-        key: 'X-Frame-Options',
-        value: 'SAMEORIGIN',
-      },
-    ];
-
-    headers.forEach(({ key, value }) => res.setHeader(key, value));
-
+    // if (isServer) {
     const showcase = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/v1/showcase/`,
       {
@@ -49,7 +18,6 @@ export const getServerSideProps = wrapper.getServerSideProps(
             process.env.NODE_ENV === 'production' && req?.headers.host
               ? req.headers.host
               : DEV_HOST,
-          'Sec-Fetch-Site': 'same-origin',
         },
       },
     ).then((response) => {
