5bece0d6190a6c3bcf31a3dbed26ffee7550fc19
eldrssn <maxkalimullin@gmail.com>
2022-12-15 22:23:08 +0500

    props to index.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,10 @@
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
-import App, { AppProps } from 'next/app';
+import {
+  // App,
+  AppProps,
+} from 'next/app';
 
 import { wrapper } from 'store';
 
@@ -11,8 +14,8 @@ import { MetrikScript } from 'utility/utils/metriks';
 
 import 'slick-carousel/slick/slick.scss';
 import 'styles/globals.scss';
-import { DEV_HOST } from 'constants/variables';
-import { fetchShowcase } from 'store/reducers/showcase/actions';
+// import { DEV_HOST } from 'constants/variables';
+// import { fetchShowcase } from 'store/reducers/showcase/actions';
 
 const theme = createTheme();
 
@@ -35,35 +38,5 @@ function MyApp({ Component, pageProps }: AppProps) {
     </>
   );
 }
-
-MyApp.getInitialProps = wrapper.getInitialAppProps(
-  (store) => async (context) => {
-    // const { ctx } = context;
-    // const isServer = !!ctx.req;
-
-    // if (isServer) {
-    const showcase = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/v1/showcase/`,
-      {
-        headers: {
-          'content-type': 'application/json',
-          'X-Client-Host': DEV_HOST,
-        },
-      },
-    ).then((response) => {
-      return response.json();
-    });
-
-    store.dispatch(fetchShowcase(showcase));
-    // }
-
-    return {
-      pageProps: {
-        ...(await App.getInitialProps(context)).pageProps,
-        appProp: context.ctx.pathname,
-      },
-    };
-  },
-);
 
 export default wrapper.withRedux(MyApp);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,28 @@
 import HomePage from 'components/base/home';
+import { DEV_HOST } from 'constants/variables';
+import { wrapper } from 'store';
+import { fetchShowcase } from 'store/reducers/showcase/actions';
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async () => {
+    const showcase = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/v1/showcase/`,
+      {
+        headers: {
+          'content-type': 'application/json',
+          'X-Client-Host': DEV_HOST,
+        },
+      },
+    ).then((response) => {
+      return response.json();
+    });
+
+    store.dispatch(fetchShowcase(showcase));
+
+    return {
+      props: {},
+    };
+  },
+);
 
 export default HomePage;
